a37d3b7e63c6aa50e8a8c7c404629e0baeadf220
Villa <bryan.m.villa.c@gmail.com>
2024-12-18 11:48:41 -0500

    Paquete1_Ejemplo04

--- Ejemplosc3/src/paquete01/Ejemplo04.java
+++ Ejemplosc3/src/paquete01/Ejemplo04.java
@@ -28,7 +28,7 @@ public class Ejemplo04 {
         };
         
         int suma;
-        
+        int sumaTotal=0;
         for (int fila = 0; fila < arreglo1.length; fila++) { // 0<2 // 1<2
             suma = 0;
             for (int col = 0; col < arreglo1[fila].length; col++) { // arreglo1[0].length:3 
@@ -38,10 +38,11 @@ public class Ejemplo04 {
                      // 10 + arreglo1[0][1] // 10 + 20 // 30
                      // 30 + arreglo1[0][2] // 30 + 40 // 70
             }
+            sumaTotal=sumaTotal+suma;
             System.out.printf("Suma de valores del arreglo: %d\n", suma);
         }
         
-        // System.out.printf("Suma de valores del arreglo: %d\n", suma);
+         System.out.printf("Suma total de valores del arreglo: %d\n", sumaTotal);
     }
     
 }
